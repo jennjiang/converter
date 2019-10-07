35f73f06f4573c6c77131dc23a3f198a534e79cd
jenn jiang <yanjunjiang@cca.edu>
2019-10-07 14:21:22 -0700

    pushing project

---
+++ converter.js
@@ -0,0 +1,15 @@
+var yargs = require("yargs");
+var arguments = yargs.argv;
+var input = argv["_"][0];
+
+// var input = Array.from(arguments);
+
+// function convertToFahrenheit(input) {
+//   var input;
+//   fahrenheit = (input * 9) / 5 + 32;
+//   return fahrenheit;
+// }
+
+// console.log(arguments["_"]);
+console.log(arguments);
+// console.log(input);
