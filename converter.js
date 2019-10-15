5738a9dba54da93a5aee2dd8a7c9102bb6d83e2c
jenn jiang <yanjunjiang@cca.edu>
2019-10-15 11:16:18 -0700

    pushing project

--- converter.js
+++ converter.js
@@ -1,24 +1,25 @@
 var argv = require("yargs")
-  .example("$0 add a numeber")
-  .example("converter.js 16")
-  .example("converter.js 12").argv;
+  .example("$0 16")
+  .example("converter.js 32 f")
+  .example("converter.js 42 c").argv;
 
 const yargs = require("yargs");
 const arguments = yargs.argv;
 const input = arguments["_"][0];
+var degree = arguments["_"][1];
 
 function convertToFahrenheit(input) {
   let fahrenheit = (input * 9) / 5 + 32;
   return fahrenheit;
 }
 
-function convert(input,degree) {
+function convert(input, degree) {
   var deg = degree || "F";
   var temp = convertToFahrenheit(input);
+
   console.log(temp + `º${deg}`);
 }
-
 // console.log(arguments["_"]);
 // console.log(arguments);
 // console.log(input);
-console.log(convert(input,degree));
+convert(input, degree);
